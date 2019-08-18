cb6b8280cdf1ad026e3f10b4dd48c2bc80abd5d8
Luciane Tedesco <lucianetedesco25@gmail.com>
2019-08-18 10:27:30 -0300

    Ajustes nas questões

--- Exercicio 02/Questao2.py
+++ Exercicio 02/Questao2.py
@@ -1,53 +1,50 @@
 import psycopg2
 try:
 
-  # Conecta no database
+  # Cria uma conexão com o banco de dados
   connection = psycopg2.connect(user = "postgres",
-                                  password = "4560",
-                                  host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "pg_db")
+                                password = "postgres",
+                                host="localhost",
+                                port = "5432",
+                                database = "pg_db")
   cursor = connection.cursor()
-    
-  
-  # Cria a tabela com nome idade e telefone
-  cursor.execute("CREATE TABLE pessoa (id_pessoa int, nm_pessoa varchar(50), ds_pessoa varchar(50), nr_idade int, PRIMARY KEY(id_pessoa));")
-  
-  # Popular a tabela
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (1, 'Simão', 'Chamado Pedro, o chefe do grupo', 30);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (2, 'André', 'Irmão de Pedro', 33);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (3, 'Tiago', 'Filho de Zebedeu', 30);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (4, 'João', 'Irmão de Tiado', 24);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (5, 'Felipe', '-', 30);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (6, 'Bartolomeu', 'Considerado como Natanael', 25);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (7, 'Tomé', 'O incrédulo', 29);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (8, 'Mateus', 'O publicano', 31);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (9, 'Tiago', 'Filho de Alfeu', 26);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (10, 'Tadeu', '-', 26);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (11, 'Simão Cananeu', 'O zelote', 28);")
-  cursor.execute("INSERT INTO pessoa (id_pessoa, nm_pessoa, ds_pessoa, nr_idade) VALUES (12, 'Judas Iscariotes', 'Aquele que o traiu e foi substituído por Matias', 30);")
 
-  # Prevenção de SqlInjection
-  possible_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'z', 
-  						'á', 'ã', 'à', 'â', 'í', 'ì', 'î', 'ú', 'ù', 'û', 'é', 'è', 'ê', 'ó', 'ò', 'õ', 'ô']
-  name = input('Digite o nome: ')
+  # Cria a tabela professor com os campos nome, titulação e departamento
+  cursor.execute("CREATE TABLE professor (id_professor int, nm_professor varchar(50), ds_titulacao varchar(30), ds_departamento varchar(30), PRIMARY KEY(id_professor));")
+  
+  # Popula a tabela
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (1, 'Gilvan Justino', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (2, 'Aurélio Faustino Hoppe', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (3, 'Georges Cherry Rodrigues', 'Mestrado', 'Matemática');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (4, 'Luciana Pereira de Araújo Kohler', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (5, 'Everaldo Artur Grahl', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (6, 'Francisco Adell Péricas', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (7, 'Andreza Sartori', 'Doutorado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (8, 'Dalton Solano dos Reis', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (9, 'Marcel Hugo', 'Mestrado', 'Sistemas e Computação');")
+  cursor.execute("INSERT INTO professor (id_professor, nm_professor, ds_titulacao, ds_departamento) VALUES (10, 'Joyce Martins', 'Mestrado', 'Sistemas e Computação');")
+  
+  # Prevenção de Sql Injection com lista branca
+  possible_characters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'z','á', 'ã', 'à', 'â', 'í', 'ì', 'î', 'ú', 'ù', 'û', 'é', 'è', 'ê', 'ó', 'ò', 'õ', 'ô']
+  name = input('Digite o nome do professor: ')
 
   for letter in name.lower():
   	if letter not in possible_characters:
   		raise Exception("Caractere \'" + letter + "\' não permitido")
   		
-  cursor.execute("SELECT * FROM pessoa WHERE nm_pessoa = '%s';" % name)
+  cursor.execute("SELECT * FROM professor WHERE nm_professor = '%s';" % name)
   
+  # Recupera e mostra os registros
+  print('Registro(s): ', cursor.rowcount)
   for row in cursor.fetchall():
     print (row)
 
-except (Exception) as error :
-    print ("Error:", error)
-
+except (Exception, psycopg2.Error) as error:
+  print ("Erro ao conectar no PostgreSQL.", error)
+  
 finally:
-    #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL: conexão fechada com sucesso.")
-
+# Fecha a conexão
+  if (connection):
+    cursor.close()
+    connection.close()
+    print("PostgreSQL: conexão fechada com sucesso.")
