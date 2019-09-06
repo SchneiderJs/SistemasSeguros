331b27f757fe613f955d746b7fbb2cd4725c27a7
Gabriel Schneider <gabriel.schneider88@hotmail.com>
2019-09-06 19:34:14 -0300

    Atualizacao com Unzip

--- Trabalho 1/user/Program.py
+++ Trabalho 1/user/Program.py
@@ -1,9 +1,21 @@
+import os
 import requests
+from zipfile import ZipFile
 
-url  = "http://127.0.0.1:5000/"
-files = {'file': open('../server_data/Atualizacao', 'rb')}
+def request_actualization():
+    url  = "http://127.0.0.1:5000/"
+    files = {'file': open('../server_data/Atualizacao.zip', 'rb')}
+    
+    new_file_with_path = os.getcwd() + "/Atualizacao.zip"
+    json = {"name": new_file_with_path}
 
-json = {"name": "C:/Users/Schneider/Documents/Estudos/Universidade/FURB/Fase_6/DST/SistemasSeguros/Trabalho 1/user/Atualizacao.txt"}
-response = requests.post(url, files=files, data=json) 
+    return requests.post(url, files=files, data=json) 
 
-print(response)
+
+def unzip(file_name = "Atualizacao.zip"):
+    with ZipFile(file_name, 'r') as zip:
+        zip.extractall()
+    os.remove(file_name)
+
+print(request_actualization())
+unzip()
